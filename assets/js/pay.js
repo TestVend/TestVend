353b4604349fbe1637b3c30233ce2a96635de48b
testvend <zohocrm1997@gmail.com>
2019-07-05 13:48:22 +0530

    no message

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -34,6 +34,8 @@ function sendObjectToVendForFiscalPrint(object) {
 // specified, and l**ater retrieved via the REST API.
 function acceptStep(receiptHTML, transactionID, qrCode, verificationUrl) {
     console.log('sending ACCEPT step')
+    var receipt = "<h2><pre>" + receiptHTML.split("======== END OF FISCAL INVOICE =========")[0] + '\r\n<a href="' + verificationUrl + '"><img src="data:image/png;base64,' + qrCode + '"/></a>' + "\r\n\r\n======== END OF FISCAL INVOICE =========" + "</pre></h2>";
+    console.log(receipt);
     sendObjectToVend({
         step: 'ACCEPT',      
         transaction_id: transactionID,
